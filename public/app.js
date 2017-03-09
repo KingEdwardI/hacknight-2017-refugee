58105a99662dd35492e787cbedbf65a921aad023
achin4bacon <kendra.leigh.cooper@gmail.com>
2017-03-08 20:42:11 -0700

    added location to front

--- public/app.js
+++ public/app.js
@@ -10,9 +10,9 @@ function closeNav() {
     document.body.style.backgroundColor = "white";
 }
 
-function appendText(imageURL,title, author,date, content, docLink){
+function appendText(imageURL,title, author,date, content, docLink, location){
    var imageDiv = "<div class='col-lg-8' id='imgdiv'><img class='sizeImg img-rounded' src='" + imageURL + "'/></div>" ;               // Create element with HTML  
-   var contentDiv =  "<div class='col-md-4' id='texxxt'><span id='title'><h2><a href='" + docLink+ "'>" + title + "</a></h2></span><br><span id='author'>" + author + "</span><br><span><sub>" + date + "</sub></span><br><br><span id='article'>" + content + "</span></div>" ;  
+   var contentDiv =  "<div class='col-md-4' id='texxxt'><span id='title'><h2><a href='" + docLink+ "'>" + title + "</a></h2></span><br><span id='author'>" + author + "</span><br><span><sub>Publication Date:" + date + "</sub><br><sub>Location:"+ location  +"</sub></span><br><br><span id='article'>" + content + "</span></div>" ;  
 
 
    var box = "<div class='row' id='"+ docLink + "'>" + imageDiv + contentDiv + "</div>"
@@ -37,9 +37,9 @@ $( document ).ready(function(){
                 obj.title = title + "....";
              }
             if(obj.image){
-                appendText(obj.image, obj.title, obj.source, obj.pubDate, obj.snippet, obj.url);
+                appendText(obj.image, obj.title, obj.source, obj.pubDate, obj.snippet, obj.url, obj.location);
             }else{
-                appendText("http://cdn.dailyheadlines.net/wp-content/uploads/2016/12/f41a8570f98033234e38d8be706b27c6.jpg", obj.title, obj.source, obj.pubDate, obj.snippet, obj.url);
+                appendText("http://cdn.dailyheadlines.net/wp-content/uploads/2016/12/f41a8570f98033234e38d8be706b27c6.jpg", obj.title, obj.source, obj.pubDate, obj.snippet, obj.url, obj.location);
             }
 
 
