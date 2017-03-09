49229446a0f8ef6b3f07dba07b8d71d9de39a338
Edward Vetter-Drake <edward.vetterdrake@gmail.com>
2017-03-08 19:05:09 -0700

    added request and filtering GET results

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
-
-
 var express = require("express");
+var request = require('request');
 
 var app = express();
 
@@ -21,6 +20,33 @@ app.get("/", function(req, res) {
 	res.sendFile(__dirname + "/index.html");
 });
 
+request.get({
+  url: "https://api.nytimes.com/svc/search/v2/articlesearch.json",
+  qs: {
+    'api-key': "fa3d1b231c1e42149ce9c29c797c6457",
+    'q': "refugee"
+  },
+}, function(err, res, body) {
+  body = JSON.parse(body)
+  articles = [];
+  docs = body.response.docs
+  docs.forEach((doc) => {
+    var article = {
+      url: doc.web_url,
+      snippet: doc.lead_paragraph,
+      source: doc.source,
+      title: doc.headline.main,
+      pubDate: doc.pub_date,
+      images: doc.multimedia.find((i) => {
+        if (i.type === 'image') {
+          return i.url;
+        }
+      })
+    }
+    console.log(article);
+  })
+
+})
 
 app.use(express.static("public"));
 // app.use(express.static("joey"));
